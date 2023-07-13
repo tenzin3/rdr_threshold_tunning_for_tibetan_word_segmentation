e1739723eff97962dcbb49825ea8729b8d39acce
Tenzin Tsundue <tenzintsunduebhattu@gmail.com>
2023-07-13 17:40:41 +0530

    'functionality_for_training_and_testing_wrong_tagged_wordtag_output_for_a_threshold_in_a_file'

--- src/word_segmentation_rules_generator/RDRPOSTagger/Utility/Eval.py
+++ src/word_segmentation_rules_generator/RDRPOSTagger/Utility/Eval.py
@@ -8,7 +8,42 @@ from .Utils import getWordTag, readDictionary
 # sys.path.append(os.path.abspath(""))
 # os.chdir("./Utility")
 
+def computeAccuracy_and_return_wrong_tagged_results(goldStandardCorpus, taggedCorpus):
+    tagged = open(taggedCorpus, encoding="utf-8").read().split()
+    goldStandard = open(goldStandardCorpus, encoding="utf-8").read().split()
+    if len(tagged) != len(goldStandard):
+        print(
+            "The numbers of word tokens in {} and {} are not equal!".format(
+                goldStandardCorpus, taggedCorpus
+            )
+        )
+        return 0
+    numwords = 0
+    count = 0
+    wrong_tagged_wordtag = ''
+    for i in range(len(tagged)):
+        numwords += 1
+        word1, tag1 = getWordTag(tagged[i])
+        word2, tag2 = getWordTag(goldStandard[i])
+        pattern = r"[་]+"
+        word1 = re.sub(pattern, "་", word1)
+        word2 = re.sub(pattern, "་", word2)
+        if word1 != word2 and word1 != "''" and word2 != "''":
+            print(
+                "Words are not the same in gold standard and tagged corpora, at the index",
+                i,
+            )
+            return 0
 
+        if tag1.lower() == tag2.lower():
+            count += 1
+        else:
+            wrong_tagged_wordtag += goldStandard[i]+'  '+tagged[i]+'\n'
+
+    return wrong_tagged_wordtag, count * 100.0 / numwords
+
+
+    
 def computeAccuracy(goldStandardCorpus, taggedCorpus):
     tagged = open(taggedCorpus, encoding="utf-8").read().split()
     goldStandard = open(goldStandardCorpus, encoding="utf-8").read().split()
--- src/word_segmentation_rules_generator/eval_rdr_result/eval_rdr_result.py
+++ src/word_segmentation_rules_generator/eval_rdr_result/eval_rdr_result.py
@@ -1,6 +1,6 @@
 import os
 
-from ..RDRPOSTagger.Utility.Eval import computeAccuracy
+from ..RDRPOSTagger.Utility.Eval import computeAccuracy, computeAccuracy_and_return_wrong_tagged_results
 
 
 def eval_rdr_result(
@@ -20,3 +20,21 @@ def eval_rdr_result(
     taggedCorpus_file_path = os.path.join(current_dir, taggedCorpus_relative_path)
 
     return computeAccuracy(goldCorpus_file_path, taggedCorpus_file_path)
+
+def eval_rdr_result_and_return_wrong_tagged(
+    goldStandardCorpus="TIB_train_maxmatched_tagged.txt",
+    taggedCorpus="TIB_train_maxmatched.txt.TAGGED",
+):
+    """
+    Input: Two files i)goldStandardCorpus: taggedfile from tagger.py based on botok maxmatch and gold corpus
+                     ii)taggedCorpus: result from the rdr rules and dictionary
+    Ouput: Accuracy of the tagged file
+    """
+    current_dir = os.path.dirname(__file__)
+    goldCorpus_relative_path = "../data/" + goldStandardCorpus
+    goldCorpus_file_path = os.path.join(current_dir, goldCorpus_relative_path)
+
+    taggedCorpus_relative_path = "../data/" + taggedCorpus
+    taggedCorpus_file_path = os.path.join(current_dir, taggedCorpus_relative_path)
+
+    return computeAccuracy_and_return_wrong_tagged_results(goldCorpus_file_path, taggedCorpus_file_path)
